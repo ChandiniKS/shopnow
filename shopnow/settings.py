bb0628bd37394095f926725a6e8ffb1d3dca0629
Chandini <chandiniks21@gmail.com>
2024-03-04 21:14:21 +0530

    changes in cart

--- shopnow/settings.py
+++ shopnow/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR /'media'
 LOGIN_REDIRECT_URL = '/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
